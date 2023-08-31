54ca727cb1a1d86f9fb48faf6d8c35919fe9d5b0
misterjacobo97 <101616295+misterjacobo97@users.noreply.github.com>
2023-09-01 01:19:36 +1000

    added markers to map

--- map.py
+++ map.py
@@ -56,7 +56,7 @@ async def GetMap():
         attr='© OpenStreetMap contributors © CARTO',
         prefer_canvas=True,
         zoom_start=14,
-    )
+    ).add_to(map)
 
     layercontrol = folium.LayerControl("topright", collapsed=True)
 
